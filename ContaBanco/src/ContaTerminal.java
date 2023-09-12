d43fc5c70bacdf40336c350860c3281bbeb3a704
Ricardo Silva <pauloricardo.ribeiro@thomsonreuters.com>
2023-09-12 13:32:08 -0300

    Entrega do ContaTerminal - curso Java Basico DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * <h1>Conta Banco</h1>
  * <p>
@@ -13,6 +15,25 @@
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        int numero = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo = 0;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, informe o seu nome!");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, informe a sua agencia!");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua conta!");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, informe o seu saldo inicial!");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo  " + saldo + " já está disponível para saque.");
     }
 }
