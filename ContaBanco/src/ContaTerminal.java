2239db301e8640bbc48bb1d6503c3986d0790935
Ricardo Silva <pauloricardo.ribeiro@thomsonreuters.com>
2023-09-12 13:04:47 -0300

    criando javadoc

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
+/**
+ * <h1>Conta Banco</h1>
+ * <p>
+ * <b>Note:</b> Projeto realizado com o intuito de demonstrar conhecimento nos ensinamentos passados pelo instrutor <b>Gleyson Sampaio</b> na aula de Java Basico<p>
+ * <b>ToDo List:</b> Conhecer e importar a classe Scanner<p>
+ * Exibir as mensagens para o nosso usuário<p>
+ * Obter pela scanner os valores deigitados no terminal<p>
+ * Exibir a mensagem da conta criada<p>
+ * @author Ricardo Silva
+ * @version 1.0
+ * @since 12/09/2023
+  */
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
     }
 }
