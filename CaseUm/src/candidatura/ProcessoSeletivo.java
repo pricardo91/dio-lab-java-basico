d6c2174fa2acbb2d597fd30e94f37c057aeb9837
Ricardo Silva <pauloricardo.ribeiro@thomsonreuters.com>
2023-09-14 14:12:37 -0300

    Estudos de cases

--- CaseUm/src/candidatura/ProcessoSeletivo.java
+++ CaseUm/src/candidatura/ProcessoSeletivo.java
@@ -1,17 +1,93 @@
 package candidatura;
 
-import java.util.Scanner;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("***Processo Seletivo!***");
-        Scanner scanner = new Scanner(System.in);
-        double pretencaoSalarial;
 
-        System.out.println("Qual a sua pretencão salarial? ");
-        pretencaoSalarial = scanner.nextDouble();
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
 
-        analisarCandidato(pretencaoSalarial);
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MAXIMO TENTATIVAS "
+                    + tentativasRealizadas + " REALIZADA");
+                    System.out.println("");
+
+    }
+
+    // método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO" };
+        System.out.println("Imprimindo lista de candidatos");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("Candidato nº " + indice + ": " + candidatos[indice]);
+        }
+
+        System.out.println("");
+        System.out.println("Forma abreviada!");
+
+        for (String candidato : candidatos) {
+            System.out.println("Candidato: " + candidato);
+        }
+    }
+
+    static void selecaoCandidatos() {
+
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA",
+                "DANIELA", "JORGE" };
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salario: R$" + salarioPretendido);
+            System.out.println("");
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                System.out.println("");
+                System.out.println("");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+
+    }
+
+    // Método que simula o valor pretendido
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     /**
@@ -23,6 +99,7 @@ public class ProcessoSeletivo {
      * @return Não há retornos esperados para esse método.
      * 
      */
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000;
         if (salarioBase > salarioPretendido) {
